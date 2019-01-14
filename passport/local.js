d0eb7f5caed149d5518ebbde99a565d7c231a928
Derek Houck <derek@derekhouck.com>
2019-01-14 13:39:11 -0800

    Update the Local Strategy to use '.validatePassword()'

--- passport/local.js
+++ passport/local.js
@@ -16,7 +16,9 @@ const localStrategy = new LocalStrategy((username, password, done) => {
           location: 'username'
         });
       }
-      const isValid = user.validatePassword(password);
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
       if (!isValid) {
         return Promise.reject({
           reason: 'LoginError',
