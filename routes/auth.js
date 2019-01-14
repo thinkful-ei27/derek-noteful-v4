2d15960ebffb72f8bce0fb57faa6b661dbc94219
Derek Houck <derek@derekhouck.com>
2019-01-14 13:20:49 -0800

    Create Passport Local Strategy and protected login route

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+const options = { session: false, failWithError: true };
+
+const localAuth = passport.authenticate('local', options);
+
+router.post('/login', localAuth, (req, res) => {
+  return res.json(req.user);
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const mongoose = require('mongoose');
 
 const User = require('../models/user');
 
