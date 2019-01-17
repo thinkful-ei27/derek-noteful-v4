522fed97c54cf58dbf4994e385570f26ce59a76f
Derek Houck <derek@derekhouck.com>
2019-01-17 11:55:33 -0800

    Update tests for GET /api/tags/:id

--- test/tags.test.js
+++ test/tags.test.js
@@ -123,13 +123,14 @@ describe.only('Noteful API - Tags', function () {
         .then(_data => {
           data = _data;
           return chai.request(app)
-            .get(`/api/tags/${data.id}`);
+            .get(`/api/tags/${data.id}`)
+            .set('Authorization', `Bearer ${token}`);
         })
         .then((res) => {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
           expect(res.body).to.be.an('object');
-          expect(res.body).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt');
+          expect(res.body).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt', 'userId');
           expect(res.body.id).to.equal(data.id);
           expect(res.body.name).to.equal(data.name);
           expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
@@ -140,6 +141,7 @@ describe.only('Noteful API - Tags', function () {
     it('should respond with a 400 for an invalid id', function () {
       return chai.request(app)
         .get('/api/tags/NOT-A-VALID-ID')
+        .set('Authorization', `Bearer ${token}`)
         .then(res => {
           expect(res).to.have.status(400);
           expect(res.body.message).to.equal('The `id` is not valid');
@@ -150,6 +152,7 @@ describe.only('Noteful API - Tags', function () {
       // The string "DOESNOTEXIST" is 12 bytes which is a valid Mongo ObjectId
       return chai.request(app)
         .get('/api/tags/DOESNOTEXIST')
+        .set('Authorization', `Bearer ${token}`)
         .then(res => {
           expect(res).to.have.status(404);
         });
@@ -160,7 +163,9 @@ describe.only('Noteful API - Tags', function () {
 
       return Tag.findOne()
         .then(data => {
-          return chai.request(app).get(`/api/tags/${data.id}`);
+          return chai.request(app)
+            .get(`/api/tags/${data.id}`)
+            .set('Authorization', `Bearer ${token}`);
         })
         .then(res => {
           expect(res).to.have.status(500);
